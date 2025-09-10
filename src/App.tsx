9972fcf7dd12972e3cd3c78edbf2eaa3988d2fd4
Faheem M. <102846045+Faheem-T@users.noreply.github.com>
2025-09-10 22:03:27 +0530

    User management pagination (#8)
    * Add pagination and query to user management
    
    * Fix user blocking and cleanup
    
    * Add react query devtools

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,19 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import {Toaster} from "react-hot-toast"
+import { Toaster } from "react-hot-toast";
 import { RouterProvider } from "react-router";
 import { router } from "./router";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 function App() {
   return (
-  <>
-    <QueryClientProvider client={new QueryClient()}>
-      <Toaster position="bottom-left"/>
-      <RouterProvider router={router}/>
-    </QueryClientProvider>
-  </>
-  )
+    <>
+      <QueryClientProvider client={new QueryClient()}>
+        <Toaster position="bottom-left" />
+        <RouterProvider router={router} />
+        <ReactQueryDevtools />
+      </QueryClientProvider>
+    </>
+  );
 }
 
 export default App;
---
+++ src/types/UserQueryFilters.ts
@@ -0,0 +1,4 @@
+export type UserQueryFilters = {
+  query?: string;
+  isVerified?: boolean;
+};
