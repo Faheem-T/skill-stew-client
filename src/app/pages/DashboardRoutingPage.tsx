81fce9c55d3f226c63ec615fd426bc186f49c5a5
Faheem M. <faheemmbasheer@gmail.com>
2026-02-02 12:38:48 +0530

    fix: use Navigate instead of useNavigate in dashboard routing page

--- src/app/pages/DashboardRoutingPage.tsx
+++ src/app/pages/DashboardRoutingPage.tsx
@@ -1,10 +1,10 @@
 import { useAppStore } from "@/app/store";
 import type React from "react";
-import { useNavigate } from "react-router";
+import { Navigate } from "react-router";
 
 export const DashboardRoutingPage: React.FC = () => {
   const user = useAppStore((state) => state.user)!;
-  const navigate = useNavigate();
-  navigate(`/dashboard/${user.role.toLowerCase()}`, { replace: true });
-  return null;
+  return (
+    <Navigate to={`/dashboard/${user.role.toLowerCase()}`} replace={true} />
+  );
 };
